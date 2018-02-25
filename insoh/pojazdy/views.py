b5f4d80dcf343f05d3a1825265fb5c6247265f33
bart3k1 <bart3k1@gmail.com>
2018-02-25 19:50:45 +0100

    00.3

--- insoh/pojazdy/views.py
+++ insoh/pojazdy/views.py
@@ -8,21 +8,34 @@ from django.views import View
 class PojazdyDetails(View):
     def get(self, request):
         pojazdy = Pojazdy.objects.all()
-
-        # p1.baterie.add(b1)
-        # p1.baterie.all()
-
         return render(request, 'pojazdy_details.html', {
             'pojazdy': pojazdy,
            })
-
-    def post(self, request):
-        pass
 
 
 class NowyPojazd(View):
     def get(self, request):
         return render(request, 'nowy_pojazd.html', {
+        })
+    def post(self, request):
+        self.userID = request.POST['userID']
+        self.nazwa = request.POST['nazwa']
+        self.baterie = request.POST['baterie']
+        p = Pojazdy.objects.create(userID=self.userID, nazwa=self.nazwa)
+        counter = 1
+        counterID = 5
+        for _ in range(int(self.baterie)):
+            b = Baterie.objects.create(numer=counter, batID=counterID)
+            b.inpojazd = p
+            b.save()
+            counter += 1
+            counterID += 2
+        return redirect("/pojazdy")
+
+# do zrobienie widok edycji / url i template-formularz
+class EdytujPojazd(View):
+    def get(self, request):
+        return render(request, 'edytuj_pojazd.html', {
         })
     def post(self, request):
         self.userID = request.POST['userID']
