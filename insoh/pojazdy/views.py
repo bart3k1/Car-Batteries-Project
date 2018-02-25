91d6f92285de087c8a7c11823d4fe595eb47ce96
bart3k1 <bart3k1@gmail.com>
2018-02-25 13:39:33 +0100

    000.4

--- insoh/pojazdy/views.py
+++ insoh/pojazdy/views.py
@@ -7,30 +7,15 @@ from django.views import View
 
 class PojazdyDetails(View):
     def get(self, request):
-        pojazd = Pojazdy.objects.get(id=4)
-        # dane = Dane.objects.all()
-        counter = 0
-        baterie = pojazd.bateries.all()
-        for i in baterie:
-            counter += 1
+        pojazd = Pojazdy.objects.get(id=1)
         return render(request, 'pojazdy_details.html', {
             'pojazd': pojazd,
-            'baterie': baterie,
-            'counter': counter,
+
 
         })
 
     def post(self, request):
-        self.name = request.POST['name']
-        self.surname = request.POST['surname']
-        self.phone = request.POST['phone']
-        self.notes = request.POST['notes']
-        guest = Guests.objects.create(name=self.name, surname=self.surname, phone=self.phone, notes=self.notes)
-        guest.save()
-        guests = Guests.objects.all()
-        return render(request, 'guests_details.html', {
-            'guests': guests
-        })
+        pass
 
 class NowyPojazd(View):
     def get(self, request):
@@ -45,8 +30,8 @@ class NowyPojazd(View):
         counterID = 5
         for _ in range(int(self.baterie)):
             b = Baterie.objects.create(numer=counter, batID=counterID)
+            b.inpojazd = p
             b.save()
-            p.bateries.add(b)
             counter += 1
             counterID += 2
         return redirect("/pojazdy")
